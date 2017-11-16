2aeab4301956427114d0db9827eb7b4552ac4aab
carloscalderon <cdcalderon@gmail.com>
2017-11-16 03:01:58 -0600

    Add initial components and initial navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SongsComponent } from './songs/songs.component';
+import { SongListComponent } from './songs/song-list/song-list.component';
+import { SongDetailComponent } from './songs/song-detail/song-detail.component';
+import { SongItemComponent } from './songs/song-list/song-item/song-item.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { PlaylistListComponent } from './playlists/playlist-list/playlist-list.component';
+import { PlaylistEditComponent } from './playlists/playlist-list/playlist-edit/playlist-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SongsComponent,
+    SongListComponent,
+    SongDetailComponent,
+    SongItemComponent,
+    PlaylistsComponent,
+    PlaylistListComponent,
+    PlaylistEditComponent
   ],
   imports: [
     BrowserModule
